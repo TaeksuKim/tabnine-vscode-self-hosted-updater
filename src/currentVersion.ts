340882e7e10cf09df23680435a73ff679bf6d961
Dima <dima@codota.com>
2023-03-06 23:16:55 +0200

    review

--- src/currentVersion.ts
+++ src/currentVersion.ts
@@ -1,11 +1,5 @@
-import { ExtensionContext, Extension, extensions } from "vscode";
-import { EXTENSION_SUBSTRING, SELF_HOSTED_UPDATER_VERSION_KEY } from "./consts";
-
-export async function removeCurrentVersion(
-  context: ExtensionContext
-): Promise<void> {
-  await context.globalState.update(SELF_HOSTED_UPDATER_VERSION_KEY, undefined);
-}
+import { Extension, extensions } from "vscode";
+import { EXTENSION_SUBSTRING } from "./consts";
 
 export default function currentVersion(): string | undefined {
   const tabnineExtension: Extension<unknown> | undefined = extensions.all.find(
