917e3519b193e66b9fe561565e31fb051e42377f
Amir Bilu <amirbilu@gmail.com>
2023-03-06 09:47:49 +0200

    initial commit

--- src/currentVersion.ts
+++ src/currentVersion.ts
@@ -1,6 +1,12 @@
 import { ExtensionContext } from "vscode";
 import { SELF_HOSTED_UPDATER_VERSION_KEY } from "./consts";
 
+export async function removeCurrentVersion(
+  context: ExtensionContext
+): Promise<void> {
+  await context.globalState.update(SELF_HOSTED_UPDATER_VERSION_KEY, undefined);
+}
+
 export default async function currentVersion(
   context: ExtensionContext,
   version?: string
