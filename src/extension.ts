d29ce5025661d67ecd3b2ece240a5ef630252c0b
yanirs <yanir@codota.com>
2023-03-08 17:13:46 +0200

    adding error message

--- src/extension.ts
+++ src/extension.ts
@@ -7,11 +7,18 @@ import serverUrl from "./serverUrl";
 import updateTask from "./updateTask";
 
 async function updateAndReload(serverUrl: string) {
-  const updatedVersion = await updateTask(serverUrl, currentVersion());
-  if (updatedVersion) {
-    await confirmReload("Tabnine updated");
-  } else {
-    console.log("Tabnine updater - nothing to update");
+  try {
+    const updatedVersion = await updateTask(serverUrl, currentVersion());
+    if (updatedVersion) {
+      await confirmReload("Tabnine updated");
+    } else {
+      console.log("Tabnine updater - nothing to update");
+    }
+  } catch (e) {
+    console.error("Failed to update Tabnine plugin", e);
+    void vscode.window.showErrorMessage(
+      "Failed to update Tabnine plugin (View Developer Tools for more details)"
+    );
   }
 }
 
